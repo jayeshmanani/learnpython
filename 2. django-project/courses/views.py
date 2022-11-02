181089408e547a408af43de94f62305d60321e78
Jayesh Manani <jsmanani@gmail.com>
2022-11-02 01:30:54 +0100

    added static, template

--- 2. django-project/courses/views.py
+++ 2. django-project/courses/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
-
+from .models import Course
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello World.")
+    courses = Course.objects
+    return render(request, 'courses/index.html', {'courses' : courses})
